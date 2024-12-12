c8bc734ffbb06cbf74bb6ab3747336035f9d293f
siyata dumjan <siyatadumjan098@gmail.com>
2024-12-12 15:08:29 +0545

    created onboarding screen
    onboarding screen

---
+++ lib/app.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:linkup_project/view/user/home/home_page.dart';
+import 'package:linkup_project/view/user/login_screen.dart';
+import 'package:linkup_project/view/user/onboarding/onboarding.dart';
+import 'package:linkup_project/view/user/onboarding/splash_screen.dart';
+import 'package:linkup_project/view/user/register_screen.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const SplashScreenView(),
+        '/onboarding': (context) => const OnboardingScreen(),
+        '/login': (context) => const LoginPage(),
+        '/register': (context) => const SignupPage(),
+        '/homepage': (context) => const HomePage(),
+      },
+    );
+  }
+}
